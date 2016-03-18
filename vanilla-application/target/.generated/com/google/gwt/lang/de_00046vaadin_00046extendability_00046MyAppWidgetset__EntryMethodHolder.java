506678dc671b6577cb490162a2b43c8bcd398e68
berndhopp <bernd@vaadin.com>
2016-03-18 12:45:40 +0100

    removed generated files

--- vanilla-application/target/.generated/com/google/gwt/lang/de_00046vaadin_00046extendability_00046MyAppWidgetset__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class de_00046vaadin_00046extendability_00046MyAppWidgetset__EntryMethodHolder {
-  public static final void init() {
-    // to be synthesized later
-  }
-}
--- vanilla-application/target/classes/VAADIN/widgetsets/de.vaadin.extendability.MyAppWidgetset/deferredjs/F01DC37CF3932A3B8CDA2B0063E0477F/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.de_vaadin_extendability_MyAppWidgetset.runAsyncCallback2('pdb(1601,1,g_d);_.vc=function Ogc(){S1b((!L1b&&(L1b=new X1b),L1b),this.a.d)};IUd(Th)(2);\n//# sourceURL=de.vaadin.extendability.MyAppWidgetset-2.js\n')
